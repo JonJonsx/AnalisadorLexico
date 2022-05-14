63abafd000c72802d4bab1212d7842488e57c2f4
JonJonsx <jonatan.souza@bandtec.com.br>
2022-05-13 23:41:22 -0300

    create tokeniza

--- tokeniza.py
+++ tokeniza.py
@@ -5,6 +5,7 @@ TESTE = False
 
 # caracteres usados em operadores
 OPERADORES = "%*/+-!^="
+# [\%\*\/\+\-\!\^\=]
 
 # caracteres usados em números inteiros
 DIGITOS = "0123456789"
@@ -17,9 +18,11 @@ FLOATS = DIGITOS + PONTO
 
 # caracteres usados em nomes de variáveis
 LETRAS = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# pega variavel [^\W/][\.a-zA-Z_.\d]+
 
 # abre e fecha parenteses
 ABRE_FECHA_PARENTESES = "()"
+# [()]
 
 # categorias
 OPERADOR = 1  # para operadores aritméticos e atribuição
@@ -30,16 +33,45 @@ PARENTESES = 4  # para '(' e ')
 # Whitespace characters: space, newline, horizontal tab,
 # vertical tab, form feed, carriage return
 BRANCOS = [' ', '\n', '\t', '\v', '\f', '\r']
-
+# espaçoes em branco [^a-zA-Z\+][\\n\\t\\v\\f\\r]
 # caractere que indica comentário
 COMENTARIO = "#"
 
-
+# media_prova = (p1 + p2)/2
 # ------------------------------------------------------------
-def tokeniza(exp):
-    """(str) -> list
 
-    Recebe uma string exp representando uma expressão e cria 
+
+def tokeniza(exp):
+    resultado = []
+    variavel_sep = []
+    for index, i in enumerate(exp):
+        if i in LETRAS:
+            variavel_sep.append(i)
+            if exp[index+1] in DIGITOS:
+                variavel_sep.append(exp[index+1])
+            if exp[index+1] not in LETRAS:
+                variavel_com = ''.join(variavel_sep)
+                resultado.append([variavel_com, VARIAVEL])
+                variavel_sep = []
+        elif i in OPERADORES:
+            resultado.append([i, OPERADOR])
+        elif i in ABRE_FECHA_PARENTESES:
+            resultado.append([i, PARENTESES])
+        elif i in DIGITOS:
+            if exp[index-1] not in LETRAS:
+                resultado.append([float(i), NUMERO])
+
+    return resultado
+
+    # elif i in DIGITOS:
+    #     print("teste")
+    # elif i in ABRE_FECHA_PARENTESES:
+    #     print("teste")
+    # elif i in BRANCOS:
+    #     print("teste")
+
+    """(str) -> list
+    Recebe uma string exp representando uma expressão e cria
     e retorna uma lista com os itens léxicos que formam a
     expressão.
 
@@ -48,18 +80,18 @@ def tokeniza(exp):
 
         [item, tipo]
 
-    O componente item de um token é 
+    O componente item de um token é
 
-        - um float: no caso do item ser um número; ou 
-        - um string no caso do item ser um operador ou 
+        - um float: no caso do item ser um número; ou
+        - um string no caso do item ser um operador ou
              uma variável ou um abre/fecha parenteses.
 
     O componente tipo de um token indica a sua categoria
-    (ver definição de constantes acima). 
+    (ver definição de constantes acima).
 
         - OPERADOR;
-        - NUMERO; 
-        - VARIAVEL; ou 
+        - NUMERO;
+        - VARIAVEL; ou
         - PARENTESES
 
     A funçao ignora tuo que esta na exp apos o caractere
